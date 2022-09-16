c2c369af7026ae8e3b23ff1cf1ebfb49afc1fa8b
anisa <aagermanova@gmail.com>
2022-09-15 18:56:34 -0700

    6 Routing Code splitting Lazy Suspence

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -4,6 +4,7 @@ import type {Configuration as DevServerConfigurations} from 'webpack-dev-server'
 export function buildDevServer(options: BuildOptions): DevServerConfigurations {
 return {
   port: options.port,
-  open: true
+  open: true,
+  historyApiFallback: true,
 }
 }
