626cb1e2f32289d9ae61bbce5089c68bcd8d90e3
anisa <aagermanova@gmail.com>
2022-09-17 19:00:34 -0700

    8 classNames create a git repository

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,14 @@ import './styles/index.scss';
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 import {useTheme} from './theme/useTheme';
-
+import { classNames } from './helpers/classNames/classNames';
 
 
 const App = () => {
     const {theme, toggleTheme} = useTheme()
 
     return (
-        <div className={`app ${theme}`}>
+        <div className={classNames('app', {}, [theme])}>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
             <button onClick={toggleTheme}>Change Theme</button>
