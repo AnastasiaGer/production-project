4ebdb142ec7ae63e454673916e0f1f81c367e05d
anisa <aagermanova@gmail.com>
2022-10-29 19:55:47 -0700

    Fix lint:ts

--- src/entities/Profile/ui/ProfileCard/ProfileCard.stories.tsx
+++ src/entities/Profile/ui/ProfileCard/ProfileCard.stories.tsx
@@ -4,7 +4,7 @@ import { ProfileCard } from 'entities/Profile';
 import { Country } from 'entities/Country';
 import { Currency } from 'entities/Currency';
 
-const avatar = 'https://store.playstation.com/store/api/chihiro/00_09_000/container/MT/en/19/EP4396-CUSA10659_00-AV00000000000112/image?w=320&h=320&bg_color=000000&opacity=100&_version=00_09_000'
+const avatar = 'https://store.playstation.com/store/api/chihiro/00_09_000/container/MT/en/19/EP4396-CUSA10659_00-AV00000000000112/image?w=320&h=320&bg_color=000000&opacity=100&_version=00_09_000';
 
 export default {
     title: 'entities/ProfileCard',
